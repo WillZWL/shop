f3688c658b8d09d926aaf8c44127560705ee1a5d
Handy Hon <handy.hon@eservicesgroup.com>
2015-06-05 15:41:01 +0800

    Remove mobile controllers & views

---
+++ application/config/template.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') || exit('No direct script access allowed');
+
+
+$config['template.site_path']	= FCPATH;
+
+$config['template.theme_paths'] = array('themes');
+
+$config['template.default_layout'] = "index";
+
--- application/mobile_views/checkout/payment_form.php
+++
@@ -1,4 +0,0 @@
-<img src="https://cdn.valuebasket.com/808AA1/vb/resources/images/logo.jpg" border="0">
-<br><br>
-	<form id="PensioPaymentForm" >
-	</form> 
