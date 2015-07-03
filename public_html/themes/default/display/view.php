faa90bd01f127790c250bca43ca0615172cc4785
Oswald Chan <oswald@eservicesgroup.com>
2015-07-03 17:25:13 +0800

    fix display view

--- public_html/themes/default/display/view.php
+++ public_html/themes/default/display/view.php
@@ -1,3 +1,3 @@
 <?php $this->load->view('/default/header') ?>
-<?php $this->load->view("/default/display/shipping") ?>
+<?php $this->load->view($content) ?>
 <?php $this->load->view('/default/footer') ?>
