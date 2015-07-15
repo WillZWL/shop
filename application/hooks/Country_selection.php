37dda0086e95ef934adb22cea02e7ba18b1e2cea
Handy Hon <handy.hon@eservicesgroup.com>
2015-07-15 11:25:40 +0800

    Change demo site url

--- application/hooks/Country_selection.php
+++ application/hooks/Country_selection.php
@@ -5,42 +5,20 @@ use GeoIp2\Database\Reader;
 
 class Country_selection
 {
-    private $domain = '';
     private $port = '';
-    private $sub_domain = '';
-    private $base_domain = '';
-    private $domain_suffix = '';
+    private $domain = '';
     private $country_code = '';
     private $lang = '';
 
     public function __construct()
     {
         list($this->domain, $this->port) = explode(':', $_SERVER['HTTP_HOST']);
-        $this->set_domain_component($this->domain);
-        // var_dump($_SERVER);die;
-    }
-
-    public function set_domain_component($domain)
-    {
-        $this->base_domain = config_item('base_domain');
-
-        if ($this->base_domain) {
-            $pos = stripos($this->domain, $this->base_domain);
-            if ($pos !== FALSE) {
-                $this->sub_domain = substr($this->domain, 0, $pos);
-                $this->domain_suffix = substr($this->domain, $pos + strlen($this->base_domain));
-            }
-        } else {
-            // TODO
-            // not set base domain
-        }
     }
 
     public function redirect_url()
     {
         $current_domain = $this->domain;
         $rewrite_domain = $this->get_rewrite_domain_by_country();
-
 
         if ($current_domain != $rewrite_domain) {
             $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https://" : "http://";
@@ -63,49 +41,19 @@ class Country_selection
         define(PLATFORMID, 'WEB'.$country_code);
 
         switch ($country_code) {
-            case 'AU':
-                $this->domain_suffix = '.com.au';
-                break;
-            case 'NZ':
-                $this->domain_suffix = '.co.nz';
-                break;
-            case 'SG':
-                $this->domain_suffix = '.com.sg';
+            case 'GB':
+                $this->domain = 'v2.digitaldiscount.co.uk';
                 break;
             case 'FR':
-                $this->domain_suffix = '.fr';
-                break;
-            case 'MX':
-                $this->domain_suffix = '.com.mx';
-                break;
-            case 'NL':
-                $this->domain_suffix = '.nl';
-                break;
-            case 'PH':
-                $this->domain_suffix = '.com.ph';
-                break;
-            case 'BE':
-                $this->domain_suffix = '.be';
-                break;
-            case 'IT':
-                $this->domain_suffix = '.it';
-                break;
-            case 'RU':
-                $this->domain_suffix = '.ru';
-                break;
-            case 'PT':
-                $this->domain_suffix = '.pt';
-                break;
-            case 'PL':
-                $this->domain_suffix = '.pl';
+                $this->domain = 'numeristock.fr';
                 break;
 
             default:
-                $this->domain_suffix = '.com';
+                $this->domain = 'v2.digitaldiscount.co.uk';
                 break;
         }
 
-        return $this->sub_domain . $this->base_domain . $this->domain_suffix;
+        return $this->domain;
     }
 
     public function get_country_code()
