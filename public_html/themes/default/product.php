27289f7162d0065fbe3ef83f5490e4c10be43e35
issac <issac.ip@eservicesgroup.com>
2015-07-16 12:31:18 +0800

    update product

--- public_html/themes/default/product.php
+++ public_html/themes/default/product.php
@@ -1,3 +1,4 @@
 <?php $this->load->view('/default/header') ?>
 <?php $this->load->view('/default/product/detail.php', $prod_info) ?>
+<?php $this->load->view('/default/product/related_product') ?>
 <?php $this->load->view('/default/footer') ?>
