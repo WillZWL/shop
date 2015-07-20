8abcd19ebf04f983012c2deec54f3cc28a2ca18a
Oswald Chan <oswald@eservicesgroup.com>
2015-07-20 13:40:37 +0800

    add tnt logo

--- public_html/themes/default/display/shipping.php
+++ public_html/themes/default/display/shipping.php
@@ -6,6 +6,7 @@
                         &nbsp;<img src="/images/shipping/dpd.jpg" border="0">
                         &nbsp;<img src="/images/shipping/royalmail.jpg" border="0">
                         &nbsp;<img src="/images/shipping/ups.jpg" border="0">
+                        &nbsp;<img src="/images/shipping/tnt.jpg" border="0">
                     <h3>Taxes and Duties</h3>
                     <p>
                         Worried about hidden costs? Please don't be, we'll take care of any applicable taxes and duties prior to delivery so the price on the website is the final amount you pay. Rest assured that your buying experience with us will be completely hassle free.
