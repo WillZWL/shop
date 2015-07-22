f3e1fd304262de0227f8de45dbce1af2999dfcc9
Handy Hon <handy.hon@eservicesgroup.com>
2015-07-22 17:35:26 +0800

    fix bug

--- public_html/themes/default/contact.php
+++ public_html/themes/default/contact.php
@@ -1,27 +1,21 @@
 <?php $this->load->view('/default/header') ?>
     <div id="contact-container">
         <h1><?= _('CONTACT US') ?></h1>
-        <h4><?= _('We’re here to help you!') ?></h4>
+        <h4><?= _("We're here to help you!") ?></h4>
         <div class="text-block">
-            <?= _('Our principal route for all enquiries is via our Sales and Customer Care Team, which has gained extensive experience and knowledge of our products and services in order to serve our customers better. ') ?>
-            <?= _('Our goal is to assist you with your queries in a friendly and helpful manner. ') ?>
+            <?= _('Our principal route for all enquiries is via our Sales and Customer Care Team, which has gained extensive experience and knowledge of our products and services in order to serve our customers better.') ?>
+             <?= _('Our goal is to assist you with your queries in a friendly and helpful manner.') ?>
         </div>
         <div class="text-block address">
-            <?= _('DigitalDiscount.co.uk
-                   Flat/RM 12, Tower 8,
-                   25/F Langham Place Office
-                   Argyle Street,
-                   Kowloon,
-                   Hong Kong
-            ') ?>
+            <?= _('DigitalDiscount.co.uk Flat/RM 12, Tower 8, 25/F Langham Place Office Argyle Street, Kowloon, Hong Kong') ?>
             <br>
             <br>
             <?= _('Tel:') ?> 0870 295 9128
         </div>
         <h4><?= _('Email Enquiries') ?></h4>
         <div class="text-block">
-            <?= _('Our ticketing system is designed to ensure that your queries are responded by the most qualified staff and as quickly as possible. ') ?>
-            <?= _('In order to take advantage of this, please kindly choose the correct department and the most relevant "Query". ') ?>
+            <?= _('Our ticketing system is designed to ensure that your queries are responded by the most qualified staff and as quickly as possible.') ?>
+             <?= _('In order to take advantage of this, please kindly choose the correct department and the most relevant "Query".') ?>
         </div>
         <div class="query-form">
             <form method="post" action="/contact/queryForm">
