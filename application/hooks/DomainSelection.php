3f71347ac2bb98399221502db56b03938b5e6634
Handy Hon <handy.hon@eservicesgroup.com>
2015-08-04 11:22:27 +0800

    remove Domain redirect by IP

--- application/hooks/DomainSelection.php
+++ application/hooks/DomainSelection.php
@@ -1,5 +1,4 @@
 <?php
-
 use GeoIp2\Database\Reader;
 
 class DomainSelection
@@ -20,97 +19,9 @@ class DomainSelection
 
     public function checkDomain()
     {
-        $currentDomain = $this->getDomain();
-        $redirectDomain = $this->getDomainByCountry();
-
-        // if ($currentDomain != $redirectDomain) {
-        //     $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https://" : "http://";
-        //
-        //     if ($_SERVER['SERVER_PORT'] != '80') {
-        //         $newUrl = $protocol . $redirectDomain . ':' . $_SERVER['SERVER_PORT'] . $_SERVER['REQUEST_URI'];
-        //     } else {
-        //         $newUrl = $protocol . $redirectDomain . $_SERVER['REQUEST_URI'];
-        //     }
-        //
-        //     header("Location: " . $newUrl);
-        //     exit;
-        // }
-
-        $this->setLocalization();
-
         $_SESSION['countryCodeFromHook'] = $this->getCountryCode();
     }
 
-    private function getDomainByCountry()
-    {
-        $countryCode = strtoupper($this->getCountryCode());
-
-        define('PLATFORMID', 'WEB'.$countryCode);
-
-        switch ($countryCode) {
-            case 'GB':
-                $domain = 'v2.digitaldiscount.co.uk';
-                $this->setLang('en_GB');
-                define(SITENAME, 'Digital Discount');
-                define(LOGO, 'digitaldiscount.png');
-                break;
-            case 'ES':
-                $domain = 'v2.buholoco.es';
-                $this->setLang('es_ES');
-                define(SITENAME, 'Buholoco');
-                define(LOGO, 'buholoco.png');
-                break;
-            case 'FR':
-                $domain = 'v2.numeristock.fr';
-                $this->setLang('fr_FR');
-                define(SITENAME, 'Numeri Stock');
-                define(LOGO, 'numeristock.jpg');
-                break;
-            case 'BE':
-                $domain = 'v2.numeristock.be';
-                $this->setLang('fr_BE');
-                define(SITENAME, 'Numeri Stock');
-                define(LOGO, 'numeristock.jpg');
-                break;
-            case 'AU':
-                $domain = 'v2.aheaddigital.net';
-                $this->setLang('en_GB');
-                define(SITENAME, 'Aheaddigital');
-                define(LOGO, 'aheaddigital.jpg');
-                break;
-            case 'NZ':
-                $domain = 'v2.aheaddigital.co.nz';
-                $this->setLang('en_GB');
-                define(SITENAME, 'Aheaddigital');
-                define(LOGO, 'aheaddigital.jpg');
-                break;
-            case 'PL':
-                $domain = 'v2.elektroraj.pl';
-                $this->setLang('pl_PL');
-                define(SITENAME, 'elektroraj');
-                define(LOGO, 'elektroraj.jpg');
-                break;
-            case 'IT':
-                $domain = 'v2.nuovadigitale.it';
-                $this->setLang('it_IT');
-                define(SITENAME, 'nuovadigitale');
-                define(LOGO, 'nuovadigitale.jpg');
-                break;
-
-            default:
-                $domain = 'v2.digitaldiscount.co.uk';
-                define(SITENAME, 'Digital Discount');
-                define(LOGO, 'digitaldiscount.png');
-                break;
-        }
-
-        return $domain;
-    }
-
-    /**
-     * Return country code
-     * @return string
-     */
     public function convertIp2Country($ip = '')
     {
         $countryCode = '';
@@ -135,45 +46,21 @@ class DomainSelection
         return $countryCode;
     }
 
-    public function setLocalization()
-    {
-        setcookie('lang', $this->getLang(), time()+3600, '/', $this->getDomain());
-        putenv('LANG=' . $this->getLang());
-        setlocale(LC_MESSAGES, $this->getLang());
-        setlocale(LC_NUMERIC, 'en_US');
-
-        $domain = 'message';
-        bindtextdomain($domain, I18N."Locale");
-
-        bind_textdomain_codeset($domain, 'UTF-8');
-
-        textdomain($domain);
-    }
-
-    /**
-     * @return string
-     */
     public function getDomain()
     {
         return $this->domain;
     }
 
-    /**
-     * @param string $domain
-     */
     public function setDomain($domain)
     {
         $this->domain = $domain;
     }
 
-    /**
-     * @return string
-     */
     public function getCountryCode()
     {
         if ($_GET['ip']) {
             $this->setCountryCode($this->convertIp2Country());
-        } elseif(isset($_COOKIE['countryCode'])) {
+        } elseif (isset($_COOKIE['countryCode'])) {
             $this->setCountryCode($_COOKIE['countryCode']);
         } else {
             $this->setCountryCode($this->convertIp2Country());
@@ -182,28 +69,18 @@ class DomainSelection
         return $this->countryCode;
     }
 
-    /**
-     * @param string $countryCode
-     */
     public function setCountryCode($countryCode)
     {
         $this->countryCode = $countryCode;
     }
 
-    /**
-     * @return string
-     */
     public function getLang()
     {
         return $this->lang;
     }
 
-    /**
-     * @param string $lang
-     */
     public function setLang($lang)
     {
         $this->lang = $lang;
     }
-
 }
