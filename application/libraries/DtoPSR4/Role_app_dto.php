81084f7920654bcf84197e5f41e004fd5a793021
brave <brave.liu@eservicesgroup.com>
2015-08-24 10:24:29 +0800

    9

---
+++ application/libraries/DtoPSR4/Role_app_dto.php
@@ -0,0 +1,96 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+include_once "Base_dto.php";
+
+class Role_app_dto extends Base_dto
+{
+
+    //class variable
+    private $id;
+    private $app_name;
+    private $url;
+    private $parent_app_id;
+    private $description;
+    private $display_order;
+    private $role_id;
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    //instance method
+    public function get_id()
+    {
+        return $this->id;
+    }
+
+    public function set_id($value)
+    {
+        $this->id = $value;
+    }
+
+    public function get_url()
+    {
+        return $this->url;
+    }
+
+    public function set_url($value)
+    {
+        $this->url = $value;
+    }
+
+    public function get_app_name()
+    {
+        return $this->app_name;
+    }
+
+    public function set_app_name($value)
+    {
+        $this->app_name = $value;
+    }
+
+    public function get_parent_app_id()
+    {
+        return $this->parent_app_id;
+    }
+
+    public function set_parent_app_id($value)
+    {
+        $this->parent_app_id = $value;
+    }
+
+    public function get_description()
+    {
+        return $this->description;
+    }
+
+    public function set_description($value)
+    {
+        $this->description = $value;
+    }
+
+    public function get_display_order()
+    {
+        return $this->display_order;
+    }
+
+    public function set_display_order($value)
+    {
+        $this->display_order = $value;
+    }
+
+    public function get_role_id()
+    {
+        return $this->role_id;
+    }
+
+    public function set_role_id($value)
+    {
+        $this->role_id = $value;
+    }
+
+}
+
+
