a19996b571e310d4cfd040c6c5318eeabf426fdc
brave <brave.liu@eservicesgroup.com>
2015-09-07 15:38:56 +0800

    add vo

---
+++ application/libraries/VoPSR4/FtpInfoVo.php
@@ -0,0 +1,160 @@
+<?php
+class FtpInfoVo extends \BaseVo
+{
+    private $id;
+    private $name;
+    private $server;
+    private $username;
+    private $password;
+    private $port;
+    private $pasv;
+    private $create_on = '0000-00-00 00:00:00';
+    private $create_at = '2130706433';
+    private $create_by = 'system';
+    private $modify_on = 'CURRENT_TIMESTAMP';
+    private $modify_at = '2130706433';
+    private $modify_by = 'system';
+
+    private $primary_key = ['id'];
+    private $increment_field = 'id';
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setServer($server)
+    {
+        $this->server = $server;
+    }
+
+    public function getServer()
+    {
+        return $this->server;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPort($port)
+    {
+        $this->port = $port;
+    }
+
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    public function setPasv($pasv)
+    {
+        $this->pasv = $pasv;
+    }
+
+    public function getPasv()
+    {
+        return $this->pasv;
+    }
+
+    public function setCreateOn($create_on)
+    {
+        $this->create_on = $create_on;
+    }
+
+    public function getCreateOn()
+    {
+        return $this->create_on;
+    }
+
+    public function setCreateAt($create_at)
+    {
+        $this->create_at = $create_at;
+    }
+
+    public function getCreateAt()
+    {
+        return $this->create_at;
+    }
+
+    public function setCreateBy($create_by)
+    {
+        $this->create_by = $create_by;
+    }
+
+    public function getCreateBy()
+    {
+        return $this->create_by;
+    }
+
+    public function setModifyOn($modify_on)
+    {
+        $this->modify_on = $modify_on;
+    }
+
+    public function getModifyOn()
+    {
+        return $this->modify_on;
+    }
+
+    public function setModifyAt($modify_at)
+    {
+        $this->modify_at = $modify_at;
+    }
+
+    public function getModifyAt()
+    {
+        return $this->modify_at;
+    }
+
+    public function setModifyBy($modify_by)
+    {
+        $this->modify_by = $modify_by;
+    }
+
+    public function getModifyBy()
+    {
+        return $this->modify_by;
+    }
+
+    public function getPrimaryKey()
+    {
+        return $this->primary_key;
+    }
+
+    public function getIncrementField()
+    {
+        return $this->increment_field;
+    }
+}
