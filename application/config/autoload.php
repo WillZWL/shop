8f2b27c096235b336ce3237041af4f6d6ca69263
Handy Hon <handy.hon@eservicesgroup.com>
2015-09-08 21:18:45 +0800

    fix bug

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -60,7 +60,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array('encrypt', 'pagination');
+$autoload['libraries'] = array('encrypt');
 
 
 /*
