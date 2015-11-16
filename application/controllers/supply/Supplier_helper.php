bfabfcd1678b883b5a1a8f9c726379bfc84de191
brave <brave.liu@eservicesgroup.com>
2015-11-16 18:06:01 +0800

    SBF#8296 Promotion Admin Request

--- application/controllers/supply/Supplier_helper.php
+++ application/controllers/supply/Supplier_helper.php
@@ -7,11 +7,6 @@ class Supplier_helper extends MY_Controller
     public function __construct()
     {
         parent::__construct(FALSE);
-        $this->load->model('supply/supplier_model');
-        $this->load->helper('url');
-        $this->load->helper('notice');
-        $this->load->helper('object');
-        $this->load->library('service/pagination_service');
     }
 
     public function js_supplist($active_only = 0)
@@ -24,13 +19,13 @@ class Supplier_helper extends MY_Controller
         $offset = 60 * 60 * 24;
         $ExpStr = "Expires: " . gmdate("D, d M Y H:i:s", time() + $offset) . " GMT";
         header($ExpStr);
-        $objlist = $this->supplier_model->get_list("dao", $where, array("orderby" => "name ASC", "limit" => -1));
+        $objlist = $this->sc['Supplier']->getDao('Supplier')->getList($where, ["orderby" => "name ASC", "limit" => -1]);
         foreach ($objlist as $obj) {
-            $sid = $obj->get_id();
-            $name = str_replace("'", "\'", $obj->get_name());
-            $sourcing_region = str_replace("'", "\'", $obj->get_sourcing_reg());
-            $currency_id = str_replace("'", "\'", $obj->get_currency_id());
-            $creditor = str_replace("'", "\'", $obj->get_creditor());
+            $sid = $obj->getId();
+            $name = str_replace("'", "\'", $obj->getName());
+            $sourcing_region = str_replace("'", "\'", $obj->getSourcingReg());
+            $currency_id = str_replace("'", "\'", $obj->getCurrencyId());
+            $creditor = str_replace("'", "\'", $obj->getCreditor());
             $slist[] = "'" . $sid . "':['" . $name . "', '" . $currency_id . "','" . $sourcing_region . "','" . $creditor . "']";
         }
         $js = "supplist = {" . implode(", ", $slist) . "};";
@@ -63,10 +58,10 @@ class Supplier_helper extends MY_Controller
         $offset = 60 * 60 * 24;
         $ExpStr = "Expires: " . gmdate("D, d M Y H:i:s", time() + $offset) . " GMT";
         header($ExpStr);
-        $objlist = $this->supplier_model->get_service_list("currency", array(), array("orderby" => "id ASC"));
+        $objlist = $this->sc['Currency']->getDao('Currency')->getList([], ["orderby" => "id ASC"]);
         foreach ($objlist as $obj) {
-            $sid = $obj->get_id();
-            $name = $obj->get_name();
+            $sid = $obj->getCurrencyId();
+            $name = $obj->getName();
             $slist[] = "'" . $sid . "':'" . $name . "'";
         }
         $js = "currencylist = {" . implode(", ", $slist) . "};";
