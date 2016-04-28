74d736a0bd22e05a47ef3c2d60868ade9972b502
brave <brave.liu@eservicesgroup.com>
2016-04-28 16:57:27 +0800

    SBF#9583 Remove Digital Discount Review Widget on Unrelated Websites

--- public_html/themes/default/product/detail.php
+++ public_html/themes/default/product/detail.php
@@ -18,7 +18,7 @@
             <div id="content">
                 <div class="product-info">
                     <div class="row">
-                        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 image-container">
+                        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 image-container">
                             <!--<div class="image">
                                 <span class="product-label exist"><span class="product-label-special"><?= _('Sale') ?></span></span>
                                 <a href="<?= get_image_file($image, 'l', $sku)?>" class="imagezoom">
@@ -57,7 +57,7 @@
                                 </div>
                             <?php endif ?>
                         </div>
-                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
                             <div class="product-info-bg">
                                 <div itemscope itemtype="http://schema.org/Product">
                                     <meta itemprop="brand" content="<?= $microdata['brand'] ?>" />
@@ -135,28 +135,6 @@
                                 <div> <?= _('4-7 working days delivery') ?> </div>
                                 <div class="tags">
                                 </div>
-                            </div>
-                        </div>
-                        <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
-                          <link href="http://www.reviewcentre.com/css/seo_badge.v3.css" rel="stylesheet" type="text/css">
-                            <script type="text/javascript" src="http://www.reviewcentre.com/js/RC.SeoBadge.v3.min.js"></script>
-                            <script type="text/javascript">RC.Badge.initialize("http://www.reviewcentre.com", 3709767)</script>
-                            <div id="rc-badge-wrapper" class="style-150x100 color-gray" style="width: 150px; height: 100px; ">
-                            <div class="rc-top-corners"></div>
-                            <div class="rc-content">
-                            <div class="rc-logo">
-                            <a title="Review Centre - Consumer Reviews" href="http://www.reviewcentre.com">http://www.reviewcentre.com</a>
-                            </div>
-                            <p class="rc-rating"></p>
-                            <div class="rc-stars"></div>
-                            <div class="rc-overview">
-                            <p class="rc-category"><a href="http://www.reviewcentre.com/products977.html" rel="nofollow">Online Electronic Shops</a></p>
-                            <p class="rc-item"><a href="http://www.reviewcentre.com/Online-Electronic-Shops/Digital-Discount-www-digitaldiscount-co-uk-reviews_3709767" rel="nofollow">Digital Discount - www.digitaldiscount.co.uk</a></p>
-                            <p class="rc-date"></p>
-                            <p class="rc-extract"></p>
-                            </div>
-                            </div>
-                            <div class="rc-write-review"><a href="http://www.reviewcentre.com/write-a-review-3709767.html" rel="nofollow">Write a review</a></div>
                             </div>
                         </div>
                         <!-- End div bg -->
