cb7a9de6d9e2dfa250f1a8fa7891447cbabfaf60
brave <brave.liu@eservicesgroup.com>
2016-05-12 16:45:41 +0800

    Fixed bug for user_vo

--- application/libraries/VoPSR4/UserVo.php
+++ application/libraries/VoPSR4/UserVo.php
@@ -7,14 +7,7 @@ class UserVo extends \BaseVo
     protected $email;
     protected $status = '0';
     protected $failed_attempt = '0';
-    protected $create_on = '0000-00-00 00:00:00';
-    protected $create_at;
-    protected $create_by;
-    protected $modify_on = '';
-    protected $modify_at;
-    protected $modify_by;
 
-    protected $primary_key = ['id'];
     protected $increment_field = '';
 
     public function setId($id)
@@ -94,76 +87,6 @@ class UserVo extends \BaseVo
         if ($create_on !== null) {
             $this->create_on = $create_on;
         }
-    }
-
-    public function getCreateOn()
-    {
-        return $this->create_on;
-    }
-
-    public function setCreateAt($create_at)
-    {
-        if ($create_at !== null) {
-            $this->create_at = $create_at;
-        }
-    }
-
-    public function getCreateAt()
-    {
-        return $this->create_at;
-    }
-
-    public function setCreateBy($create_by)
-    {
-        if ($create_by !== null) {
-            $this->create_by = $create_by;
-        }
-    }
-
-    public function getCreateBy()
-    {
-        return $this->create_by;
-    }
-
-    public function setModifyOn($modify_on)
-    {
-        if ($modify_on !== null) {
-            $this->modify_on = $modify_on;
-        }
-    }
-
-    public function getModifyOn()
-    {
-        return $this->modify_on;
-    }
-
-    public function setModifyAt($modify_at)
-    {
-        if ($modify_at !== null) {
-            $this->modify_at = $modify_at;
-        }
-    }
-
-    public function getModifyAt()
-    {
-        return $this->modify_at;
-    }
-
-    public function setModifyBy($modify_by)
-    {
-        if ($modify_by !== null) {
-            $this->modify_by = $modify_by;
-        }
-    }
-
-    public function getModifyBy()
-    {
-        return $this->modify_by;
-    }
-
-    public function getPrimaryKey()
-    {
-        return $this->primary_key;
     }
 
     public function getIncrementField()
