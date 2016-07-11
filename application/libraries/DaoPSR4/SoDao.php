03eb43522e9a338633d208cd81d729737852dc8f
brave <brave.liu@eservicesgroup.com>
2016-07-11 09:54:58 +0800

    Aftership update

--- application/libraries/DaoPSR4/SoDao.php
+++ application/libraries/DaoPSR4/SoDao.php
@@ -2941,7 +2941,7 @@ SQL;
         $this->db->join("client c", "c.id = so.client_id", "INNER");
         $this->db->join("so_shipment sosh", "soa.sh_no = sosh.sh_no", "LEFT");
         $this->db->join("country cy", "cy.country_id = so.bill_country_id", "INNER");
-        $this->db->join("order_status_history osh", "osh.so_no = so.so_no", "INNER");
+        $this->db->join("order_status_history osh", "osh.so_no = so.so_no AND osh.status = 6", "INNER");
         $this->db->group_by("so.so_no");
         return $this->commonGetList($classname, $where, $option, 'sosh.tracking_no trackingno, sosh.courier_id courier, so.so_no so_no, so.bill_name bill_name, c.email clientemail, cy.id_3_digit country_code, so.dispatch_date');
     }
